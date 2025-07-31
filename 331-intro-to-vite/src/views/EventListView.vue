6805972813e30613b428a5abbfccbf41f097d274
Rattikan <Rattikan_m@cmu.ac.th>
2025-07-31 20:48:08 +0700

    2.3 Apply Tailwind Flexbox classes

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -46,7 +46,7 @@ onMounted(() => {
 <template>
   <h1>Events For Good</h1>
   <!-- new element-->
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
   <div class="page-info">
@@ -78,11 +78,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
   width: 290px;
