354c62445a231ffe952afb352669e00f3d14ce27
Rattikan <Rattikan_m@cmu.ac.th>
2025-08-01 01:07:14 +0700

    use spread operator for conditional devtools plugin

--- 331-intro-to-vite/vite.config.ts
+++ 331-intro-to-vite/vite.config.ts
@@ -3,27 +3,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => {
-  const plugins = [
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    // เพิ่ม plugin ของ Vue เสมอ
     vue(),
-  ];
-
-  if (mode === 'development') {
-    plugins.push(VueDevTools());
-  }
-
-  return {
-    base: '/',
-    plugins: plugins,
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    build: {
-      rollupOptions: {
-        external: ['nprogress/nprogress.css']
-      }
+    // ใช้ Spread Operator เพื่อเพิ่ม VueDevTools เข้าไปใน array
+    // โดยถ้าอยู่ในโหมด 'development' จะเพิ่ม array ที่มี VueDevTools() เข้าไป
+    // ถ้าไม่อยู่ในโหมด 'development' (เช่น 'production') จะเพิ่ม array ว่าง [] เข้าไปแทน
+    ...(mode === 'development' ? [VueDevTools()] : []),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  };
-});
+  },
+  build: {
+    rollupOptions: {
+      external: ['nprogress/nprogress.css']
+    }
+  }
+}));
